2cfd405e4dff084079ab98ef8bf999009231526e
prince.bhagat <prince.bhagat@ria.insure>
2023-11-25 23:50:41 +0530

    added requst param

--- src/main/java/com/prince/cache/controller/Cache.java
+++ src/main/java/com/prince/cache/controller/Cache.java
@@ -4,12 +4,13 @@ package com.prince.cache.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Cache {
     @GetMapping("/count")
-    ResponseEntity<Integer> count(){
-        return ResponseEntity.ok(100);
+    ResponseEntity<Integer> count(@RequestParam Integer val){
+        return ResponseEntity.ok(val);
     }
 }
 
